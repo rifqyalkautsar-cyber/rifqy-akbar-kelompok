abfe8abc2136e6fd23f78664387f78b8c133c084
AkbarFaisall <muhammadakbarfaisal1@gmail.com>
2026-05-04 11:49:41 +0800

    Akbar132_#3: Fitur Read dan Delete Task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
-    //
+    //Fitur Read [GET]
+    public function index()
+    {
+        $tasks = Task::all();
+        
+        return response()->json([
+            'message' => 'Berhasil mengambil semua data task',
+            'data' => $tasks
+        ], 200);
+    }
+
+    //Fitur Delete [DELETE]
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+
+        if (!$task) {
+            return response()->json([
+                'message' => 'Task tidak ditemukan'
+            ], 404);
+        }
+
+        $task->delete();
+
+        return response()->json([
+            'message' => 'Task berhasil dihapus'
+        ], 200);
+    }
 }
