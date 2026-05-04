9da46c2b1d451e3e97122b005a50f3770495ece1
rifqyalkautsar-cyber <rifqyalkautsar06@gmail.com>
2026-05-04 12:43:06 +0800

    Update TaskController.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,27 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Task; // <-- Ini sangat penting agar file Controller mengenali Model Task
+use App\Models\Task;
 
 class TaskController extends Controller
 {
-    // Fitur Create [POST] - Dikerjakan oleh Rifqy
+    // Fitur Read [GET] - Punya Akbar
+    public function index()
+    {
+        $tasks = Task::all();
+        
+        return response()->json([
+            'message' => 'Berhasil mengambil semua data task',
+            'data' => $tasks
+        ], 200);
+    }
+
+    // Fitur Create [POST] - Punya Rifqy
     public function store(Request $request)
     {
-        // Validasi data yang masuk (wajib diisi, berupa teks, maksimal 255 huruf)
         $request->validate([
             'title' => 'required|string|max:255'
         ]);
 
-        // Simpan data ke database
         $task = Task::create([
             'title' => $request->title
         ]);
 
-        // Berikan jawaban (response) bahwa data sukses disimpan
         return response()->json([
             'message' => 'Task berhasil ditambahkan!',
             'data' => $task
         ], 201);
+    }
+
+    // Fitur Delete [DELETE] - Punya Akbar
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        
+        if ($task) {
+            $task->delete();
+            return response()->json(['message' => 'Task berhasil dihapus']);
+        }
+        
+        return response()->json(['message' => 'Task tidak ditemukan'], 404);
     }
 }
