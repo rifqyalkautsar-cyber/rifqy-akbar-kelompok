8cbba5bd871fc0962504e975c93d349e047305c7
rifqyalkautsar-cyber <rifqyalkautsar06@gmail.com>
2026-05-04 12:42:30 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController; 
+use App\Http\Controllers\TaskController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
+// Fitur Create (Punya Rifqy)
 Route::post('/tasks', [TaskController::class, 'store']);
+
+// Fitur Read & Delete (Punya Akbar)
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
